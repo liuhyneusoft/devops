7fc640f0b2e7bc03b16b0b2977897344211eb65f
HaiYangLiu <dllhyg@cn.ibm.com>
2020-05-25 17:19:15 +0800

    git

---
+++ src/test/java/com/devops/githubs/GithubsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.githubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
